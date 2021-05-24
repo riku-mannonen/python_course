89c1d3b113a0b5097a6b056cd3586053a0e5cb62
Riku <bgm059@myy.haaga-helia.fi>
2021-05-24 12:27:21 +0300

    Add template

---
+++ flask/hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("base.html")
+
+@app.route("/toinenreitti")
+def toinen_sivu():
+        return "Hei toisesta reitistä!"
+
+app.run(debug=True)
