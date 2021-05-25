7c6ab4873ac08a93a1205f80c58776b1d013c6fe
riku-mannonen <82582724+riku-mannonen@users.noreply.github.com>
2021-05-25 10:20:24 +0300

    Delete flask/kotitehtävät1 directory

--- flask/kotitehtävät1/painoindeksilaskuri.py
+++
@@ -1,7 +0,0 @@
-def bmi(weight, height):
-    return weight / height ** 2
-
-if __name__ == "__main__":
-	weight = float(input('enter weight (kg): ' ))
-	height = float(input('enter height (m): '))
-	print(f'you\'re bmi is {bmi(weight, height)}')
