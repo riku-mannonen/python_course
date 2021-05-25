8968600831a5fb70fd4202f2f47708777a7e5b64
Riku <bgm059@myy.haaga-helia.fi>
2021-05-25 14:00:54 +0300

    Add forms

--- flask/day2/forms.py
+++ flask/day2/forms.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route("/")
+def forms():
+        return render_template("forms3.html")
+
 @app.route("/post")
 def post():
 	return render_template("forms.html")
