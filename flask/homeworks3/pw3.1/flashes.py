f64955f719e26a9a1c0b98f613f86c220e757c7f
Riku <bgm059@myy.haaga-helia.fi>
2021-05-26 21:45:07 +0300

    Add homework

---
+++ flask/homeworks3/pw3.1/flashes.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, flash
+
+app = Flask(__name__)
+app.secret_key = "lLX3Zi01q5&UL0=)aaXaebegnw6%3aet9rus"
+@app.route("/")
+def index():
+    return render_template("base.html")
+
+@app.route("/one")
+def one():
+    flash("Valar morghulis")
+    return redirect("/")
+
+@app.route("/two")
+def two():
+    flash("Valar dohaeris")
+    return redirect("/")
+
+if __name__ == "__main__":
+	app.run()
