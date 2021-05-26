854d5530fb20ccf18137861203a6206cb82f67d9
Riku <bgm059@myy.haaga-helia.fi>
2021-05-26 14:20:28 +0300

    Edit file

--- flask/day3/orm.py
+++ flask/day3/orm.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
@@ -8,35 +8,39 @@ app = Flask(__name__)
 app.secret_key = "aiX3Zi0umohRiXaebegaer3aet9eng"
 db = SQLAlchemy(app)
 
-class Comment(db.Model):
+class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
-    feeling = db.Column(db.String)
+    genre = db.Column(db.String)
 
-CommentForm = model_form(Comment, base_class=FlaskForm, db_session=db.session)
+MovieForm = model_form(Movie, base_class=FlaskForm, db_session=db.session)
 
 @app.before_first_request
 def initMe():
 	db.create_all()
-	comment = Comment(text="Great day!", name="Tero")
-	db.session.add(comment)
+	movie = Movie(name="Demons", genre="horror")
+	db.session.add(movie)
 
-	comment = Comment(text="Cloudy", name="Erkki Esimerkki")
-	db.session.add(comment)
+	movie = Movie(name="Great Silence", genre="western")
+	db.session.add(movie)
 
 	db.session.commit()
 
+@app.route("/msg")
+def msgPage():
+    flash("Here is a message for you!")
+    return redirect("/")
+
 @app.route("/new", methods=["GET", "POST"])
 def addForm():
-    form = CommentForm()
+    form = MovieForm()
     print(request.form) # test only
     return render_template("new.html", form=form)
 
 @app.route("/")
 def index():
-    comments = Comment.query.all()
-    return render_template("base.html", comments=comments)
+    movies = Movie.query.all()
+    return render_template("index.html", movies=movies)
 
 if __name__ == "__main__":
 	app.run()
